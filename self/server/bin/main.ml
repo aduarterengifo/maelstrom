ea0057d853abcf9279a40b55bdf6784a212b6b9f
adr <andres@duarterengifo.com>
2025-05-30 18:39:42 -0400

    save, we are going concurrent!

--- self/server/bin/main.ml
+++ self/server/bin/main.ml
@@ -94,7 +94,12 @@ let request_body_of_yojson (json : Yojson.Safe.t) : request_body =
       TopologyBody { msg_id; topology }
   | `Broadcast -> 
       let msg_id = json |> member "msg_id" |> to_int in
-      let message = json |> member "message" |> to_string in
+    let message =
+      match json |> member "message" with
+      | `String s -> s
+      | `Int i -> string_of_int i
+      | v -> failwith ("Unexpected type for message: " ^ Yojson.Safe.to_string v)
+      in
       BroadcastBody { msg_id; message }
   | `Read -> 
       let msg_id = json |> member "msg_id" |> to_int in
@@ -309,8 +314,165 @@ type read_response = {
   body: read_response_body
 } [@@deriving yojson]
 
+type state = {
+  mutable node_id : string;
+  mutable msg_id : int;
+  mutable messages : string list;
+  mutable neighbors : string list;
+}
+
+let handle_message line node_id msg_id messages neighbors = 
+  Eio.Flow.copy_string (Printf.sprintf "[RECEIVED] %s\n" line) stderr;
+        let json_str = Yojson.Safe.from_string line in
+        let req = request_of_yojson json_str in 
+        match req.body with
+        | InitBody b -> 
+            Eio.Flow.copy_string "[MATCH] InitBody\n" stderr;
+            (* construct init response *) 
+            let response_str = {
+              src = b.node_id;
+              dest = req.src;
+              body = {
+                type_ = "init_ok";
+                msg_id = msg_id;
+                in_reply_to = b.msg_id;
+              }
+            } 
+              |> yojson_of_init_response 
+              |> Yojson.Safe.to_string in
+            (* log to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "Initialized node %s\n" b.node_id) stderr;
+            Eio.Flow.copy_string (Printf.sprintf "Init send %s\n" response_str) stderr;
+            (* msg init response *) 
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+            (* handle init, update node_id *)
+            loop b.node_id (msg_id + 1) [] neighbors
+        | EchoBody b ->
+            Eio.Flow.copy_string "[MATCH] EchoBody\n" stderr;
+            (* construct echo response *) 
+            let response_str = {
+              src = req.dest;
+              dest = req.src;
+              body = {
+                type_ = "echo_ok";
+                msg_id = Some msg_id;
+                in_reply_to = b.msg_id;
+                echo = b.echo;
+              }
+            } 
+              |> yojson_of_echo_response 
+              |> Yojson.Safe.to_string in
+            (* log to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "Echoing %s\n" response_str) stderr;
+            (* msg echo response *)
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+            loop node_id (msg_id + 1) [] neighbors
+        | GenerateBody b ->             
+            Eio.Flow.copy_string "[MATCH] GenerateBody\n" stderr;
+            let response_str = {
+              src = node_id;
+              dest = req.src;
+              body = {
+                type_ = "generate_ok";
+                msg_id = Some msg_id;
+                in_reply_to = b.msg_id;
+                id = node_id ^ string_of_int msg_id;
+              }
+            } 
+              |> yojson_of_generate_response 
+              |> Yojson.Safe.to_string in
+            (* log to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "[GENERATE RESPONSE] %s\n" response_str) stderr;
+            (* msg echo response *)
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+            loop node_id (msg_id + 1) messages neighbors
+        | TopologyBody b ->            
+            Eio.Flow.copy_string "[MATCH] TopologyBody\n" stderr;
+            let response_str = {
+              src = node_id;
+              dest = req.src;
+              body = {
+                type_ = "topology_ok";
+                msg_id = Some msg_id;
+                in_reply_to = b.msg_id;
+              }
+            } 
+              |> yojson_of_topology_response 
+              |> Yojson.Safe.to_string in
+            (* log response to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "[TOPOLOGY RESPONSE] %s\n" response_str) stderr;
+            (* msg echo response *)
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+            loop node_id (msg_id + 1) messages (Hashtbl.find b.topology node_id)
+            (* TODO: handle not found *)
+        | BroadcastBody b ->             
+            Eio.Flow.copy_string "[MATCH] BroadcastBody\n" stderr;
+            let response_str = {
+              src = node_id;
+              dest = req.src;
+              body = {
+                type_ = "broadcast_ok";
+                msg_id = Some msg_id;
+                in_reply_to = b.msg_id;
+              }
+            } 
+              |> yojson_of_broadcast_response 
+              |> Yojson.Safe.to_string in
+            (* log to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "[BROADCAST RESPONSE] %s\n" response_str) stderr;
+            (* msg echo response *)
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+
+            
+
+            (* BROADCAST TO NEIGHBORS *)
+            if not (List.mem b.message messages) then (
+              Eio.Flow.copy_string "[INSIDE BROADCAST LOOP]\n" stderr;
+              List.iter (fun neighbor -> 
+                let request: request = {
+                  id = None;
+                  src = node_id;
+                  dest = neighbor;
+                  body = BroadcastBody { message = b.message; msg_id }
+                } in
+                let request_str = yojson_of_request request |> Yojson.Safe.to_string in
+                (* log to stderr *)
+                Eio.Flow.copy_string (Printf.sprintf "[BROADCAST REQUEST] %s\n" request_str) stderr;
+                (* msg echo response *)
+                Eio.Flow.copy_string (request_str ^ "\n") stdout;
+              
+                ) neighbors;
+            );
+            (* BROADCAST TO NEIGHBORS *)
+
+            loop node_id (msg_id + 1) (b.message :: messages) neighbors
+        | ReadBody b ->             
+            Eio.Flow.copy_string "[MATCH] ReadBody\n" stderr;
+            let response_str = {
+              src = node_id;
+              dest = req.src;
+              body = {
+                type_ = "read_ok";
+                msg_id = Some msg_id;
+                in_reply_to = b.msg_id;
+                messages
+              }
+            } 
+              |> yojson_of_read_response 
+              |> Yojson.Safe.to_string in
+            (* log to stderr *)
+            Eio.Flow.copy_string (Printf.sprintf "[READ RESPONSE] %s\n" response_str) stderr;
+            (* msg echo response *)
+            Eio.Flow.copy_string (response_str ^ "\n") stdout;
+            loop node_id (msg_id + 1) messages neighbors
+        | UnknownBody b -> 
+            Eio.Flow.copy_string "[MATCH] UnknownBody\n" stderr;
+            (* maybe at some point implement the errors *)
+
 let main ~stdin ~stdout ~stderr =
   let buf = Eio.Buf_read.of_flow stdin ~max_size:4096 in
+  let state = { messages = []; neighbors = [] } in
+  let state_mutex = Eio.Mutex.create () in
   Eio.Flow.copy_string "[INFO] Entering main loop\n" stderr;
   let rec loop node_id msg_id messages neighbors =
     match Eio.Buf_read.line buf with
@@ -420,18 +582,21 @@ let main ~stdin ~stdout ~stderr =
 
             (* BROADCAST TO NEIGHBORS *)
             if not (List.mem b.message messages) then (
-              let request: request = {
-                id = None;
-                src = node_id;
-                dest = req.src;
-                body = BroadcastBody { message = b.message; msg_id }
-              } in
-              let request_str = yojson_of_request request |> Yojson.Safe.to_string in
-
-              (* log to stderr *)
-              Eio.Flow.copy_string (Printf.sprintf "[BROADCAST REQUEST] %s\n" request_str) stderr;
-              (* msg echo response *)
-              Eio.Flow.copy_string (request_str ^ "\n") stdout;
+              Eio.Flow.copy_string "[INSIDE BROADCAST LOOP]\n" stderr;
+              List.iter (fun neighbor -> 
+                let request: request = {
+                  id = None;
+                  src = node_id;
+                  dest = neighbor;
+                  body = BroadcastBody { message = b.message; msg_id }
+                } in
+                let request_str = yojson_of_request request |> Yojson.Safe.to_string in
+                (* log to stderr *)
+                Eio.Flow.copy_string (Printf.sprintf "[BROADCAST REQUEST] %s\n" request_str) stderr;
+                (* msg echo response *)
+                Eio.Flow.copy_string (request_str ^ "\n") stdout;
+              
+                ) neighbors;
             );
             (* BROADCAST TO NEIGHBORS *)
 
