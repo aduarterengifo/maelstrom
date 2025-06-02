cf4f95240996d8128305c63f0b3158a99ddf12c2
adr <andres@duarterengifo.com>
2025-06-02 17:02:52 -0400

    v confused

--- self/server/bin/main.ml
+++ self/server/bin/main.ml
@@ -30,7 +30,7 @@ type topology_request_body = {
 type broadcast_request_body = {
   message: string;
   msg_id: int option;
-}
+} [@@deriving yojson]
 
 type read_request_body = {
   msg_id: int option;
@@ -268,78 +268,59 @@ type request = {
 
 
 type response_body =
-  | Init of init_response_body
-  | Echo of echo_response_body
-  | Generate of generate_response_body
-  | Topology of topology_response_body
-  | Broadcast of broadcast_response_body
-  | Read of read_response_body
+  | InitOk of init_response_body
+  | EchoOk of echo_response_body
+  | GenerateOk of generate_response_body
+  | TopologyOk of topology_response_body
+  | BroadcastOk of broadcast_response_body
+  | ReadOk of read_response_body
   | Error of response_error_body
+  | Broadcast of broadcast_request_body 
 
 let response_body_of_yojson (json : Yojson.Safe.t) : response_body =
   let open Yojson.Safe.Util in
   let type_str = json |> member "type" |> to_string in
   match type_str with
-  | "init_ok" -> Init (init_response_body_of_yojson json)
-  | "echo_ok" -> Echo (echo_response_body_of_yojson json)
-  | "generate_ok" -> Generate (generate_response_body_of_yojson json)
-  | "topology_ok" -> Topology (topology_response_body_of_yojson json)
-  | "broadcast_ok" -> Broadcast (broadcast_response_body_of_yojson json)
-  | "read_ok" -> Read (read_response_body_of_yojson json)
+  | "init_ok" -> InitOk (init_response_body_of_yojson json)
+  | "echo_ok" -> EchoOk (echo_response_body_of_yojson json)
+  | "generate_ok" -> GenerateOk (generate_response_body_of_yojson json)
+  | "topology_ok" -> TopologyOk (topology_response_body_of_yojson json)
+  | "broadcast_ok" -> BroadcastOk (broadcast_response_body_of_yojson json)
+  | "read_ok" -> ReadOk (read_response_body_of_yojson json)
   | "error" -> Error (response_error_body_of_yojson json)
   | s -> failwith ("Unknown response type: " ^ s)
 
-let yojson_of_response_body = function
-  | Init b ->
-      let base = yojson_of_init_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "init_ok") :: fields)
-      | _ -> base)
-  | Echo b ->
-      let base = yojson_of_echo_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "echo_ok") :: fields)
-      | _ -> base)
-  | Generate b ->
-      let base = yojson_of_generate_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "generate_ok") :: fields)
-      | _ -> base)
-  | Topology b ->
-      let base = yojson_of_topology_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "topology_ok") :: fields)
-      | _ -> base)
-  | Broadcast b ->
-      let base = yojson_of_broadcast_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "broadcast_ok") :: fields)
-      | _ -> base)
-  | Read b ->
-      let base = yojson_of_read_response_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "read_ok") :: fields)
-      | _ -> base)
-  | Error b ->
-      let base = yojson_of_response_error_body b in
-      (match base with
-      | `Assoc fields -> `Assoc (("type", `String "error") :: fields)
-      | _ -> base)
+let response_body_id = function
+  | InitOk _      -> "init_ok"
+  | EchoOk _      -> "echo_ok"
+  | GenerateOk _  -> "generate_ok"
+  | TopologyOk _  -> "topology_ok"
+  | BroadcastOk _ -> "broadcast_ok"
+  | Broadcast _ -> "broadcast"
+  | ReadOk _      -> "read_ok"
+  | Error _     -> "error"
+
+let yojson_of_response_body rb =
+  let type_str = response_body_id rb in
+  let base = match rb with
+    | InitOk b      -> yojson_of_init_response_body b
+    | EchoOk b      -> yojson_of_echo_response_body b
+    | GenerateOk b  -> yojson_of_generate_response_body b
+    | TopologyOk b  -> yojson_of_topology_response_body b
+    | BroadcastOk b -> yojson_of_broadcast_response_body b
+    | ReadOk b      -> yojson_of_read_response_body b
+    | Error b       -> yojson_of_response_error_body b
+    | Broadcast b   -> yojson_of_broadcast_request_body b
+  in
+  match base with
+  | `Assoc fields -> `Assoc (("type", `String type_str) :: fields)
+  | _ -> base
 
 type response = {
   src: string;
   dest: string;
   body: response_body [@yojson.of_yojson response_body_of_yojson] [@yojson.to_yojson yojson_of_response_body];
 } [@@deriving yojson]
-
-let response_body_id = function
-  | Init _      -> "init"
-  | Echo _      -> "echo"
-  | Generate _  -> "generate"
-  | Topology _  -> "topology"
-  | Broadcast _ -> "broadcast"
-  | Read _      -> "read"
-  | Error _     -> "error"
 
 let request_body_id = function
   | InitBody _      -> "init"
@@ -353,9 +334,14 @@ let request_body_id = function
 
 type state = {
   mutable node_id : string;
+  mutable msg_id : int Atomic.t;
   mutable messages : StringSet.t;
-  mutable neighbors : string list;
+  mutable neighbors : StringSet.t;
+  callbacks : (int, request_body Eio.Promise.u) Hashtbl.t;
 }
+
+let next_msg_id state =
+  Atomic.fetch_and_add state.msg_id 1
 
 let write_line_sync ~mutex ~flow line =
   Eio.Mutex.use_rw ~protect:true mutex (fun () ->
@@ -365,7 +351,7 @@ let write_line_sync ~mutex ~flow line =
 let add_common_fields ~msg_id ~in_reply_to body =
   { body with msg_id = Some msg_id; in_reply_to = in_reply_to }
 
-let send_response_and_log ~stdout_mutex ~stdout ~stderr_mutex ~stderr response =
+let send ~stdout_mutex ~stdout ~stderr_mutex ~stderr response =
   let response_str = yojson_of_response response |> Yojson.Safe.to_string in
   let tag = response_body_id response.body in
   write_line_sync ~mutex:stderr_mutex ~flow:stderr ("[" ^ String.uppercase_ascii tag ^ "] " ^ response_str);
@@ -384,19 +370,76 @@ let make_response (req: request) state body = {
  src = state.node_id; dest = req.src; body
 }
 
+let rec gossip_to_neighbors ~state ~state_mutex ~stdout ~stderr ~stdout_mutex ~stderr_mutex ~message ~exclude =
+  let open Eio.Std in
+  let neighbors =
+    Eio.Mutex.use_ro state_mutex (fun () ->
+      StringSet.elements (StringSet.diff state.neighbors (StringSet.singleton exclude))
+    )
+  in
+  let unacked = ref (StringSet.of_list neighbors) in
+  while not (StringSet.is_empty !unacked) do
+    Eio.traceln "Need to replicate %s to %s" message (String.concat ", " (StringSet.elements !unacked));
+    StringSet.iter (fun dest ->
+      let msg_id = next_msg_id state in
+      let req = {
+        id = msg_id;
+        src = state.node_id;
+        dest;
+        body = BroadcastBody { message; msg_id = Some msg_id }
+      } in
+      let promise, resolver = Eio.Promise.create () in
+      Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
+        Hashtbl.replace state.callbacks msg_id resolver
+      );
+      let req_str = yojson_of_request req |> Yojson.Safe.to_string in
+      write_line_sync ~mutex:stderr_mutex ~flow:stderr ("[BROADCAST REQUEST: " ^ state.node_id ^ " -> " ^ dest ^ "] " ^ req_str);
+      write_line_sync ~mutex:stdout_mutex ~flow:stdout req_str;
 
-let handle_message ~line ~msg_id ~state ~state_mutex ~stdout ~stderr ~stdout_mutex ~stderr_mutex = 
+      Eio.Fiber.fork ~sw (fun () ->
+        match Eio.Promise.await_exn promise with
+        | BroadcastOk _ ->
+            Eio.traceln "Got response from %s" dest;
+            Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
+              unacked := StringSet.remove dest !unacked
+            )
+        | _ -> ()
+      )
+    ) !unacked;
+    Eio.Time.sleep (Eio.Stdenv.clock Eio.Stdenv.default) 1.0
+  done;
+  Eio.traceln "Done with message %s" message
+
+
+let handle_message ~line ~state ~state_mutex ~stdout ~stderr ~stdout_mutex ~stderr_mutex = 
   Eio.Flow.copy_string (Printf.sprintf "[RECEIVED] %s\n" line) stderr;
         let json_str = Yojson.Safe.from_string line in
         let req = request_of_yojson json_str in 
+
+        let msg_id_opt = msg_id_of_request_body req.body in
+
+          let handled_by_callback =
+            match msg_id_opt with
+            | Some msg_id ->
+                Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
+                  match Hashtbl.find_opt state.callbacks msg_id with
+                  | Some resolver ->
+                      Eio.Promise.resolve resolver req.body;
+                      Hashtbl.remove state.callbacks msg_id;
+                      true
+                  | None -> false
+                )
+            | None -> false
+          in
+
         write_line_sync ~mutex:stderr_mutex ~flow:stderr ("[MATCH " ^ String.uppercase_ascii (request_body_id req.body) ^ "]");
         let body = match req.body with
         | InitBody b -> 
             Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
               state.node_id <- b.node_id;
             );
-            let body = Init {
-                msg_id = msg_id;
+            let body = InitOk {
+                msg_id = Atomic.get state.msg_id;
                 in_reply_to = b.msg_id;
               } in 
             Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
@@ -404,28 +447,32 @@ let handle_message ~line ~msg_id ~state ~state_mutex ~stdout ~stderr ~stdout_mut
             );
             Some body
         | EchoBody b ->
-            let body = Echo {
-                msg_id = Some msg_id;
+            let body = EchoOk {
+                msg_id = Some (Atomic.get state.msg_id);
                 in_reply_to = b.msg_id;
                 echo = b.echo;
               } in
             Some body;
         | GenerateBody b ->             
-            let body = Generate {
-                msg_id = Some msg_id;
+            let body = GenerateOk {
+                msg_id = Some (Atomic.get state.msg_id);
                 in_reply_to = b.msg_id;
-                id = state.node_id ^ string_of_int msg_id;
+                id = state.node_id ^ string_of_int b.msg_id;
               } in 
             Some body;
         | TopologyBody b ->            
-            let body = Topology {
-                msg_id = Some msg_id;
+            let body = TopologyOk {
+                msg_id = Some (Atomic.get state.msg_id);
                 in_reply_to = (match b.msg_id with Some id -> id | None -> 0);
               }in
 
             Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
-              state.neighbors <- (Hashtbl.find b.topology state.node_id);
+              state.neighbors <- StringSet.of_list (Hashtbl.find b.topology state.node_id);
             );
+
+            (* in a message with *some* callback id *)
+            (* i keep an association from msg_ids to callbacks ids *)
+            (* *)
 
             Some body
             (* TODO: handle not found *)
@@ -433,67 +480,88 @@ let handle_message ~line ~msg_id ~state ~state_mutex ~stdout ~stderr ~stdout_mut
             (* locks the state *)
             (* if message is not in messages *)
             (* add it to the set *)
-            (* for each neighbor: send broadcast *)
+            (* for each neighbor (not including the one that sent it to us): send broadcast *)
             Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
                 if not (StringSet.mem b.message state.messages) then (
                   state.messages <- StringSet.add b.message state.messages;
-                  List.iter (fun neighbor -> 
-                    let request: request = {
-                      id = 0;
-                      src = state.node_id;
-                      dest = neighbor;
-                      body = BroadcastBody { message = b.message; msg_id = None }
-                    } in
-                    let request_str = yojson_of_request request |> Yojson.Safe.to_string in
-                    write_line_sync ~mutex:stderr_mutex ~flow:stderr ("[BROADCAST REQUEST: " ^ state.node_id ^ " -> " ^ neighbor ^ "] " ^ request_str);
-                    write_line_sync ~mutex:stdout_mutex ~flow:stdout request_str;
-                    ) state.neighbors;
+                (*
+                  modify to continuously send this 
+                *)
+                  StringSet.iter (fun neighbor -> 
+                      let request: request = {
+                        id = 0;
+                        src = state.node_id;
+                        dest = neighbor;
+                        body = BroadcastBody { message = b.message; msg_id = None }
+                      } in
+                      let request_str = yojson_of_request request |> Yojson.Safe.to_string in
+                      write_line_sync ~mutex:stderr_mutex ~flow:stderr ("[BROADCAST REQUEST: " ^ state.node_id ^ " -> " ^ neighbor ^ "] " ^ request_str);
+                      write_line_sync ~mutex:stdout_mutex ~flow:stdout request_str;
+                    ) (StringSet.diff state.neighbors (StringSet.singleton req.src));
                 ) 
             );
 
             (match b.msg_id with
               | Some _ ->  
-                  let body = Broadcast {
-                      msg_id = Some msg_id;
+                  let body = BroadcastOk {
+                      msg_id = Some (Atomic.get state.msg_id);
                       in_reply_to = (match b.msg_id with Some id -> id | None -> 0);
                     } in
                   Some body;
               |  None -> None;
             );
         | ReadBody b ->             
-            let body = Read {
-                msg_id = Some msg_id;
+            let body = ReadOk {
+                msg_id = Some (Atomic.get state.msg_id);
                 in_reply_to = (match b.msg_id with Some id -> id | None -> 0);
                 messages = Eio.Mutex.use_ro state_mutex (fun () -> StringSet.elements state.messages)
               } in 
             Some body
         | UnknownBody b -> 
             None
-        in 
+        in
         match body with
-        | Some body -> send_response_and_log ~stdout_mutex ~stdout ~stderr_mutex ~stderr (make_response req state body)
+        | Some body -> send ~stdout_mutex ~stdout ~stderr_mutex ~stderr (make_response req state body)
         | None -> ();
         ()
 
-        
+(* OK new understanding unlocked. the msg_id shit was not used by the other calls, it will only be used internally by us. *)
+(* set handler for map *)
+let rpc ~state ~stdout_mutex ~stdout ~stderr_mutex ~stderr ~state_mutex ~dest ~req ~req_body ~res_body = 
+  Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
+    (* increment msg_id *)
+    let msg_id = next_msg_id state in
+    (* record the req kind in the callbacks assoc *)
+    Hashtbl.replace state.callbacks msg_id (request_body_id req_body);
+    (* send response *)
+    send ~stdout_mutex ~stdout ~stderr_mutex ~stderr (make_response req state res_body)
+  ) 
+
+let send_request_with_callback ~state ~state_mutex ~msg_id ~request =
+  let promise, resolver = Eio.Promise.create () in
+  Eio.Mutex.use_rw ~protect:true state_mutex (fun () ->
+    Hashtbl.replace state.callbacks msg_id resolver;
+    (* send the request here *)
+  );
+  promise
 
 let main ~stdin ~stdout ~stderr =
   let buf = Eio.Buf_read.of_flow stdin ~max_size:4096 in
-  let state = { node_id= "NOTSET"; messages = StringSet.empty; neighbors = [] } in
+  let state = { node_id = "NOTSET"; msg_id = Atomic.make 0; messages = StringSet.empty; neighbors = StringSet.empty; callbacks = Hashtbl.create 0; } in
   let state_mutex = Eio.Mutex.create () in
   let stdout_mutex = Eio.Mutex.create () in 
   let stderr_mutex = Eio.Mutex.create () in
   Eio.Switch.run (fun sw ->
-    let rec loop msg_id =
+    let rec loop () =
       match Eio.Buf_read.line buf with
       | line ->
           Eio.Fiber.fork ~sw (fun () ->
-            handle_message ~line ~msg_id ~state ~state_mutex ~stdout ~stderr ~stdout_mutex ~stderr_mutex
+            handle_message ~line ~state ~state_mutex ~stdout ~stderr ~stdout_mutex ~stderr_mutex
           );
-          loop (msg_id + 1)
+          loop ()
       | exception End_of_file -> ()
     in
-    loop 0
+    loop ()
   )
 
 let () =
